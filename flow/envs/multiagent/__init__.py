02c710e0106de273db116c296634f00f63362f28
kevin-thankyou-lin <kevin.lin20000@gmail.com>
2020-07-04 10:29:04 +1200

    updated init file and changes bay1inf file - not 100% working

--- flow/envs/multiagent/__init__.py
+++ flow/envs/multiagent/__init__.py
@@ -8,9 +8,8 @@ from flow.envs.multiagent.ring.accel import MultiAgentAccelEnv
 from flow.envs.multiagent.traffic_light_grid import MultiTrafficLightGridPOEnv
 from flow.envs.multiagent.highway import MultiAgentHighwayPOEnv
 from flow.envs.multiagent.bayesian_0_no_grid_env import Bayesian0NoGridEnv
-
-
+from flow.envs.multiagent.bayesian_1_inference_env import Bayesian1InferenceEnv
 
 __all__ = ['MultiEnv', 'MultiAgentAccelEnv', 'MultiWaveAttenuationPOEnv',
            'MultiTrafficLightGridPOEnv', 'MultiAgentHighwayPOEnv',
-           'Bayesian0NoGridEnv']
+           'Bayesian0NoGridEnv', 'Bayesian1InferenceEnv']
